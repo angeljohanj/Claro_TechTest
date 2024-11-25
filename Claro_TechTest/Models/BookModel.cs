989038ce0e743d476f02c8eafb4db9e74982134e
angeljohanj <angeljohanjimenezcampo@gmail.com>
2024-11-25 17:08:49 -0400

    bookmodel added to proj

---
+++ Claro_TechTest/Models/BookModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Claro_TechTest.Models
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int? PageCount { get; set; }
+        public string? Excerpt { get; set; }
+        public DateTime? PublishDate { get; set; }
+
+    }
+}
